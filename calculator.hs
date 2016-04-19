74e8027759251e5d733c649e91b5710b7b35ecfc
Dan McArdle <zingermc@gmail.com>
2016-04-18 23:58:30 -0400

    Implementented half-baked parsing feature

--- calculator.hs
+++ calculator.hs
@@ -9,8 +9,30 @@ data ParseNode = UnitsNumber Float Unit
 
 type ArithFunc = Float -> Float -> Float
 
-parse :: String -> ParseNode
-parse s = error "not implemented"
+data Token = TokOp Operator
+           | TokNum Float
+           | TokUnits String
+           | TokOpenParen
+           | TokCloseParen
+           deriving (Show)
+
+tokenize :: String -> [Token]
+tokenize s = map toToken (words s)
+  where
+    toToken "+" = TokOp Plus
+    toToken "*" = TokOp Times
+    toToken "/" = TokOp Divide
+    toToken "(" = TokOpenParen
+    toToken ")" = TokCloseParen
+    toToken w
+     | all ((flip elem (['a'..'z']++['_']))) w = TokUnits w
+     | otherwise = TokNum (read w)
+
+parse :: [Token] -> ParseNode
+parse [TokNum f, TokUnits s] = UnitsNumber f s
+parse (t1:t2:t3:ts) = case (t1,t2,t3) of
+  (TokNum f, TokUnits s, TokOp op) -> Operation op (UnitsNumber f s) (parse ts)
+  (_, _, _) -> error "Bad parse"
 
 opFunc :: Operator -> (ParseNode -> ParseNode -> ParseNode)
 opFunc op = case op of
@@ -44,8 +66,6 @@ crunch   (Operation op node1 node2) = (opFunc op) (crunch node1) (crunch node2)
 
 calculator :: String -> String
 calculator s = (show value) ++ " " ++ units
-           where
-                node = parse s
-                (UnitsNumber value units) = crunch node
+           where (UnitsNumber value units) = crunch $ parse $ tokenize s
                 
 main = interact calculator
